c282328b59bd2d4852562e3c9980771fd489ba94
Martin Stoilov <martin@sigmadrone.org>
2019-04-08 23:58:25 -0700

    Added ipython notebook to test the pid controller.

--- firmware/sigmadrive/src/pidcontroller.h
+++ firmware/sigmadrive/src/pidcontroller.h
@@ -8,7 +8,9 @@ class PidController
 {
 public:
 	PidController(float kp = 0, float ki = 0, float kd = 0, float kl = 0)
-		: integral_error_(0)
+		: output_(0)
+		, differential_error_(0)
+		, integral_error_(0)
 		, last_error_input_(0)
 		, kp_(kp)
 		, ki_(ki)
@@ -27,14 +29,14 @@ public:
 	float GetKi() const 							{ return ki_; }
 	float GetKd() const 							{ return kd_; }
 	float GetKl() const								{ return kl_; }
-	T GetOutput() const								{ return output_; }
-
-	void Update(const T& error_input, float dT)
+	T Output() const								{ return output_; }
+	T Input(const T& error_input, float dT)
 	{
 		integral_error_ = (integral_error_ + error_input * dT) * (1.0 - kl_);
-		integral_error_ = (error_input - last_error_input_) / dT;
+		differential_error_ = (error_input - last_error_input_) / dT;
 		last_error_input_ = error_input;
 		output_ = kp_ * error_input + ki_ * integral_error_ + kd_ * differential_error_;
+		return Output();
 	}
 
 
@@ -47,7 +49,7 @@ private:
 	float kp_;				/* proportional gain */
 	float ki_;				/* integral gain */
 	float kd_;				/* derivative gain */
-	float kl_;	/* Integral error leak. This must be between 0.0 and 1.0 */
+	float kl_;				/* Integral error leak. This must be between 0.0 and 1.0 */
 
 };
 
--- host/boost-python/sigmadrive-ext/main.cpp
+++ host/boost-python/sigmadrive-ext/main.cpp
@@ -1,135 +1,25 @@
 #include <iostream>
 #include <boost/python.hpp>
+using namespace boost::python;
 
 #include "pidcontroller.h"
 
+
 using PidControllerFloat = PidController<float>;
 
-using namespace boost::python;
-
-class World
-{
-public:
-	World(std::string msg) :
-			msg(msg)
-	{
-	}
-
-	World(double a, double b)
-	{
-		msg = "double, double";
-	}
-	void set(std::string msg)
-	{
-		this->msg = msg;
-	}
-
-	std::string greet()
-	{
-		return msg;
-	}
-
-public:
-	std::string msg;
-};
-
-struct Var
-{
-    Var(std::string name) : name(name), value() {}
-    std::string const name;
-    float value;
-};
-
-class Num
-{
-public:
-	Num() {};
-	float get() const			{ return val_; }
-	void set(float value)		{ val_ = value; }
-
-protected:
-	float val_ = 0.0f;
-};
-
-
-struct Base
-{
-	virtual ~Base() {};
-	virtual int f() { return 0; }
-};
-
-struct Derived : Base
-{
-	virtual int f() override { return 3; }
-};
-
-
-struct BaseWrap : Base, wrapper<Base>
-{
-    int f()
-    {
-    	override f = this->get_override("f");
-        if (f)
-            return f(); // *note*
-        return Base::f();
-    }
-
-    int default_f() { return this->Base::f(); }
-};
-
-void b(Base*)
-{
-
-}
-
-void d(Derived*)
-{
-
-}
-
-Base* factory()
-{
-	return new Derived;
-}
 
 BOOST_PYTHON_MODULE(sigmadrive_ext)
 {
-    class_<PidControllerFloat>("PidController", init<float, float, float, float>())
+    class_<PidControllerFloat>("PidController")
+    	.def(init<optional<float, float, float, float> >())
+		.def("Input", &PidControllerFloat::Input)
+		.def("Output", &PidControllerFloat::Output)
         .add_property("Kp", &PidControllerFloat::GetKp, &PidControllerFloat::SetKp)
         .add_property("Ki", &PidControllerFloat::GetKi, &PidControllerFloat::SetKi)
         .add_property("Kd", &PidControllerFloat::GetKd, &PidControllerFloat::SetKd)
 		.add_property("Kl", &PidControllerFloat::GetKl, &PidControllerFloat::SetKl)
-
     ;
 
-
-    class_<World>("World", init<std::string>())
-    	.def(init<double, double>())
-        .def("greet", &World::greet)
-        .def("set", &World::set)
-    ;
-
-    class_<Var>("Var", init<std::string>())
-    	.def_readonly("name", &Var::name)
-		.def_readwrite("value", &Var::value)
-	;
-
-    class_<Num>("Num")
-    		.add_property("rovalue", &Num::get)
-			.add_property("value", &Num::get, &Num::set)
-	;
-
-    def("b", b);
-    def("d", d);
-
-    // Tell Python to take ownership of factory's result
-    def("factory", factory, return_value_policy<manage_new_object>());
-
-    class_<BaseWrap, boost::noncopyable>("Base")
-		.def("f", &Base::f, &BaseWrap::default_f)
-        ;
-
-    class_<Derived>("Derived");
 }
 
 
