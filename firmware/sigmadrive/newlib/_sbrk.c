a6013ce47f192db41817be302b5ae940404824d4
Martin Stoilov <martin@sigmadrone.org>
2019-09-15 15:42:26 -0700

    Fixed _sbrk(int incr)

--- firmware/sigmadrive/newlib/_sbrk.c
+++ firmware/sigmadrive/newlib/_sbrk.c
@@ -1,25 +1,26 @@
 #include <errno.h>
 #include <sys/types.h>
 
-caddr_t _sbrk (int incr)
+caddr_t _sbrk(int incr)
 {
-   register char * stack_ptr asm ("sp");
-   extern char end asm ("end");
-   static char *heap_end;
-   char *prev_heap_end;
+	extern char end asm ("end");
+	static char *heap_end;
+	char *prev_heap_end;
+	extern char _Heap_Limit;
 
-   if (heap_end == NULL)
-        heap_end = &end;
+	if (heap_end == NULL)
+		heap_end = &end;
 
-    prev_heap_end = heap_end;
+	prev_heap_end = heap_end;
 
-    if(heap_end + incr > stack_ptr)
-    {
-        errno = ENOMEM;
-        return (caddr_t) -1;
-    }
+	incr = (incr + 3) & (~3); // align value to 4
 
-    heap_end += incr;
+	if (heap_end + incr > &_Heap_Limit) {
+		errno = ENOMEM;
+		return (caddr_t) -1;
+	}
 
-   return (caddr_t) prev_heap_end;
+	heap_end += incr;
+	return (caddr_t) prev_heap_end;
 }
+
