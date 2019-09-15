d031922435ebcdb1a9811aedc8239f032bae410a
Martin Stoilov <martin@sigmadrone.org>
2019-09-15 15:40:29 -0700

    Added tim.h/cpp

---
+++ firmware/sigmadrive/application/tim.cpp
@@ -0,0 +1,18 @@
+/*
+ * tim.cpp
+ *
+ *  Created on: Sep 6, 2019
+ *      Author: mstoilov
+ */
+
+#include "tim.h"
+
+Tim::Tim() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Tim::~Tim() {
+	// TODO Auto-generated destructor stub
+}
+
