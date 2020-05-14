8210219ba69b54d907fb22096117a6f01a236eb6
Martin Stoilov <martin@sigmadrone.org>
2020-05-14 13:22:44 -0700

    Use ADC2 and ADC3 for current sensing.

--- firmware/sigmadrive/application/adcdata.cpp
+++ firmware/sigmadrive/application/adcdata.cpp
@@ -9,12 +9,17 @@
  */
 void AdcData::ReadPhaseCurrent(int32_t *data, size_t size)
 {
-    if (size > 0)
+    if (size > 0) {
         data[0] = (int32_t) adc1.InjReadConversionData(LL_ADC_INJ_RANK_1);
-    if (size > 1)
+    }
+    if (size > 1) {
         data[1] = (int32_t) adc1.InjReadConversionData(LL_ADC_INJ_RANK_2);
-    if (size > 2)
+//        data[1] = (int32_t) adc2.InjReadConversionData(LL_ADC_INJ_RANK_1);
+    }
+    if (size > 2) {
         data[2] = (int32_t) adc1.InjReadConversionData(LL_ADC_INJ_RANK_3);
+//        data[2] = (int32_t) adc3.InjReadConversionData(LL_ADC_INJ_RANK_1);
+    }
 }
 
 
