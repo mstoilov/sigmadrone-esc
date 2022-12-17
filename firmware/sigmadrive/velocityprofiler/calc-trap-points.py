3f099de3acf1bc0914efcd2d4533cf9eb440191a
Martin Stoilov <martin@sigmadrone.org>
2022-12-17 17:13:00 +0100

    code clean up and comments.

--- firmware/sigmadrive/velocityprofiler/calc-trap-points.py
+++ firmware/sigmadrive/velocityprofiler/calc-trap-points.py
@@ -7,9 +7,9 @@ import sys
 Pf = 1000000
 Pi = 0
 Vi = 0
-Vmax = 65535 * 30
+Vmax = 2000000
 Acc = 30000000
-Dec = 20000000
+Dec = 10000000
 HZ = 18000
 
 nargs = len(sys.argv)
@@ -22,9 +22,10 @@ Acc = int(sys.argv[5]) if nargs > 5 else Acc
 Dec = int(sys.argv[6]) if nargs > 6 else Dec
 HZ = int(sys.argv[7]) if nargs > 7 else HZ
 
-prof = tp.TrapezoidProfile();
+prof = tp.TrapezoidProfile()
 points = prof.CalcTrapPoints(Pf, Pi, Vi, Vmax, Acc, Dec, HZ)
 
+print("             Time,     Velocity,     Position")
 for i in range(0, 4):
     print("point ", i, " : ", points[i].time, ", ", points[i].velocity, ", ", points[i].position)
 
@@ -50,16 +51,19 @@ for k in range(0, 4):
     if (T2 > T1):
         A = (V2 - V1) / (T2 - T1)
         for i in range(0, point.time) :
-            v = V[i + offset] = V1  + A * i;
-            S[i + offset] = S2 - (v + V2) * (T2 - i) / 2;    
+            v = V[i + offset] = V1  + A * i
+            S[i + offset] = S2 - (v + V2) * (T2 - i) / 2
     offset += point.time
     
 
 pp.figure()
 pp.subplot(2,1,1)
-pp.plot(time, V)
+pp.plot(time, V, label="Velocity")
+pp.ylabel('Velocity')
+#pp.legend(loc="upper right")
 pp.subplot(2,1,2)
-pp.plot(time, S, color="orange")
+pp.plot(time, S, color="orange", label="Position")
 pp.xlabel('Time')
 pp.ylabel('Position')
+#pp.legend(loc="right")
 pp.show()
--- firmware/sigmadrive/velocityprofiler/trapezoid-profile.py
+++ firmware/sigmadrive/velocityprofiler/trapezoid-profile.py
@@ -9,68 +9,13 @@ def sign_hard(x):
 def SQ(x):
     return x*x
 
-
-def trapezoidalProfile(Xf, Xi, Vin, Vmax, Amax, Dmax, t):
-    Yd = 0
-    Y = 0
-    s = 1.0 if (Xf > Xi) else -1.0
-    
-    Vr = Vmax
-    Ar = Amax
-    Dr = Dmax
-    Vi = s * Vin
-    dX = np.abs(Xf - Xi)
-    
-    if (Vi > Vr):
-        Ar = -Ar
-    Da = 0.5 * (Vr + Vi) * (Vr - Vi) / Ar
-    Dd = 0.5 * SQ(Vr) / Dr
-    Dc = dX - (Da + Dd)
-    if (Dc < 0):
-        # Find Vr by solving:
-        # Da + Dd = dX
-        # 0.5 * (Vr + Vi) * (Vr - Vi) / Ar + 0.5 *(Vr * Vr) / Dr = dX
-        Vr_sq = Dr*(2.0 * Ar * dX + SQ(Vi))/(Dr + Ar)
-        #Vr_sq = (((Vi**2 + 2*Ar*dX)*(Ar + Dr))/Dr)
-        if (Vr_sq < 0 or Da == 0):
-            # The distence to the requested position is too short 
-            # for the specified decelaration.
-            # Calculate the required decelaration to reach the position
-            Vr = Vi
-            Dr = 0.5 * SQ(Vi) / dX
-        else:
-            Vr = np.sqrt(Vr_sq)
-        Dc = 0
-        Da = 0.5 * (Vr + Vi) * (Vr - Vi) / Ar
-        Dd = 0.5 * SQ(Vr) / Dr
-
-    
-    Ta = (Vr - Vi) / Ar
-    Td = Vr / Dr
-    Tc = Dc / Vr
-    T = Ta + Tc + Td
-
-    if (t < Ta):
-        Ydd = s * Ar
-        Yd = s * (Vi + Ar * t)
-        Y   = Xi + s * (Vi * t + 0.5 * Ar * SQ(t))
-    elif t < (Ta + Tc):
-        Ydd = 0
-        Yd = s * Vr
-        Y   = Xi + s * (Vi * Ta + 0.5 * Ar * SQ(Ta) + Vr * (t - Ta))
-    elif (t < T):
-        tc = t - (Ta + Tc)
-        Ydd = - s * Dr
-        Yd = s * (Vr - Dr * tc)
-        Y = Xi + s * (Vi * Ta + 0.5 * Ar * SQ(Ta) + Vr * Tc + Vr * tc - 0.5 * Dr * SQ(tc))
-    else:
-        Ydd = 0
-        Yd = 0
-        Y = Xf
-        
-    return Y, Yd, Ydd
-
-
+# Pf	Position (final)
+# Pi	Position (initial)
+# Vi	Velocity (initial) at Pi
+# Vmax	Maximum allowed velocity
+# Amax	Acceleration
+# Dmax	Deceleration
+# H		Update frequency
 Pf = 1000000
 Pi = 0
 Vi = 0
@@ -81,15 +26,15 @@ HZ = 18000
 
 nargs = len(sys.argv)
 
-Pf = int(sys.argv[1]) if nargs > 1 else Pf
-Pi = int(sys.argv[2]) if nargs > 2 else Pi
-Vi = int(sys.argv[3]) if nargs > 3 else Vi
-Vmax = int(sys.argv[4]) if nargs > 4 else Vmax
-Acc = int(sys.argv[5]) if nargs > 5 else Acc
-Dec = int(sys.argv[6]) if nargs > 6 else Dec
-HZ = int(sys.argv[7]) if nargs > 7 else HZ
+Pf = int(sys.argv[1]) if nargs > 1 else Pf				# Position (final)
+Pi = int(sys.argv[2]) if nargs > 2 else Pi				# Position (initial)
+Vi = int(sys.argv[3]) if nargs > 3 else Vi				# Velocity (initial) at Pi
+Vmax = int(sys.argv[4]) if nargs > 4 else Vmax			# Maximum allowed velocity
+Acc = int(sys.argv[5]) if nargs > 5 else Acc			# Acceleration
+Dec = int(sys.argv[6]) if nargs > 6 else Dec			# Deceleration
+HZ = int(sys.argv[7]) if nargs > 7 else HZ				# Update frequency
 
-prof = tp.TrapezoidProfile();
+prof = tp.TrapezoidProfile()
 prof.Init(Pf, Pi, Vi, Vmax, Acc, Dec, HZ)
 
 time = np.arange(0, prof.T + HZ/10)
@@ -111,5 +56,5 @@ print("Td = ", prof.Td)
 pp.plot(time, Vc * HZ)
 pp.plot(time, Sc)
 pp.xlabel('Time')
-pp.ylabel('P,Pd,Pdd')
+pp.ylabel('P,Pd')
 pp.show()
