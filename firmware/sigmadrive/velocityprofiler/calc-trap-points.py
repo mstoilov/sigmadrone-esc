6b1dd184714c7badf5f0a1cb9e9aca6d85a23062
Martin Stoilov <martin@sigmadrone.org>
2023-01-05 16:37:21 +0100

    The profiler now takes Vfin parameter.

--- firmware/sigmadrive/velocityprofiler/calc-trap-points.py
+++ firmware/sigmadrive/velocityprofiler/calc-trap-points.py
@@ -1,74 +1,126 @@
-import trapezoidprofile as tp
 import matplotlib.pyplot as pp
 import numpy as np
+import trapezoidprofile as tp
 import sys
 
 
-Pf = 1000000
-Pi = 0
-Vi = 0
-Vmax = 2000000
-Acc = 30000000
-Dec = 10000000
-HZ = 18000
+# Pin   - initial position              [ec]
+# Pfin  - final position                [ec]
+# Vin   - initial velocity              [ec/sec]
+# Vfin  - final velocity                [ec/sec]
+# Vmax  - max velocity allowed          [ec/sec]
+# Accel - acceleration                  [ec/(sec*sec)]
+# Decel - deceleration                  [ec/(sec*sec)]
+# Hz    - Closed loop update frequency  [Hz]
+def CalculateTrapezoidPoints(Pin, Pfin, Vin, Vfin, Vmax, Accel, Decel, Hz, scale=1.0):
+    squareHz = Hz * Hz
+    s = float(1.0) if Pfin >= Pin else float(-1.0)  # Direction of the trajectory
+    Vr = float(np.abs(Vmax) / Hz)                   # Requested velocity in encoder counts/per timeslice
+    Ar = float(np.abs(Accel)/squareHz)              # Requested acceleration in encoder counts/per timeslice
+    Dr = float(np.abs(Decel)/squareHz)              # Requested deceleration in encoder counts/per timeslice
+    dP = float(np.abs(Pfin - Pin))                  # Total displacement
+    Vi = float(s * Vin  / Hz)                       # Initial speed in encoder counts/per timeslice
+    Vf = float(s * Vfin  / Hz)                      # Final speed in encoder counts/per timeslice
 
-nargs = len(sys.argv)
+    if (Vi > Vr):
+        Ar = -Ar
+    Da = 0.5 * (Vr + Vi) * (Vr - Vi) / Ar           # Displacement during acceleration
+    Dd = 0.5 * (Vr + Vf) * (Vr - Vf) / Dr           # Displacement during deceleration
+    Dc = dP - (Da + Dd)                             # Displacement during const velocity
 
-Pf = int(sys.argv[1]) if nargs > 1 else Pf
-Pi = int(sys.argv[2]) if nargs > 2 else Pi
-Vi = int(sys.argv[3]) if nargs > 3 else Vi
-Vmax = int(sys.argv[4]) if nargs > 4 else Vmax
-Acc = int(sys.argv[5]) if nargs > 5 else Acc
-Dec = int(sys.argv[6]) if nargs > 6 else Dec
-HZ = int(sys.argv[7]) if nargs > 7 else HZ
+    if (Dc < 0):
+        # Find Vr by solving:
+        # Da + Dd = dX
+        # 0.5 * (Vr + Vi) * (Vr - Vi) / Ar + 0.5 *(Vr * Vr) / Dr = dX
+        Vr_sq = Dr * (2.0 * Ar * dP + np.square(Vi))/(Dr + Ar)
+        if (Vr_sq < 0 or Da == 0):
+            # The distance to the requested position is too short
+            # for the specified decelaration.
+            # Calculate the required decelaration to reach the position
+            Vr = Vi
+            Dr = 0.5 * np.square(Vi) / dP
+        else:
+            Vr = np.sqrt(Vr_sq)
+        Dc = 0
+        Da = 0.5 * (Vr + Vi) * (Vr - Vi) / Ar
+        Dd = 0.5 * (Vr + Vf) * (Vr - Vf) / Dr
 
-points = tp.CalculateTrapezoidPoints(Pf, Pi, Vi, Vmax, Acc, Dec, HZ)
+    Ta = (Vr - Vi) / Ar
+    Tr = Dc / Vr if (Vr != 0) else 0
+    Td = (Vr - Vf) / Dr
 
-print("             Time,     Velocity,     Position")
-for i in range(0, 4):
-    print("point ", i, " : ", points[i][0], ", ", points[i][1], ", ", points[i][2])
+    pt0t = 0
+    pt0v = Vi
+    pt0p = Pin
 
-totalTime = 0
-for i in range(0, 4):
-    totalTime += points[i][0]
+    pt1t = Ta
+    pt1v = s * Vr
+    pt1p = Pin + (Vi + pt1v) * Ta * 0.5
+
+    pt2t = Tr
+    pt2v = s * Vr
+    pt2p = pt1p + (pt1v + pt2v) * Tr * 0.5
+
+    pt3t = Td
+    pt3v = Vf
+    pt3p = Pfin
+
+    #
+    # Return the 4 points of the trapezoid
+    #
+    return [[pt0t, pt0v, pt0p], [pt1t, pt1v, pt1p], [pt2t, pt2v, pt2p], [pt3t, pt3v, pt3p]]
 
 
-time = np.arange(0, totalTime)
-A = np.zeros_like(time, dtype=float)
-V = np.zeros_like(time, dtype=float)
-S = np.zeros_like(time, dtype=float)
-T = np.zeros_like(time, dtype=float)
+if __name__ == "__main__":
+    nargs = len(sys.argv)
+    Pi = int(sys.argv[1]) if nargs > 1 else 0
+    Pf = int(sys.argv[2]) if nargs > 2 else 10000000
+    Vi = int(sys.argv[3]) if nargs > 3 else 0
+    Vf = int(sys.argv[4]) if nargs > 4 else 0
+    Vmax = int(sys.argv[5]) if nargs > 5 else 2000000
+    Acc = int(sys.argv[6]) if nargs > 6 else 3000000
+    Dec = int(sys.argv[7]) if nargs > 7 else 1000000
+    HZ = int(sys.argv[8]) if nargs > 8 else 18000
 
-V2 = 0
-offset = 0
+    points = np.array(tp.CalculateTrapezoidPoints(Pi, Pf, Vi, Vf, Vmax, Acc, Dec, HZ), dtype=int)
+    Tcol = points[0:5, 0]
+    Vcol = points[0:5, 1]
+    Pcol = points[0:5, 2]
 
-for k in range(0, 4):
-    point = points[k]
-    V1 = V2
-    V2 = point[1]
-    S2 = point[2]
-    T1 = 0
-    T2 = point[0]
-    if (T2 > T1):
-        Acc = (V2 - V1) / (T2 - T1)
-        for i in range(0, int(point[0])) :
-            A[i + offset] = Acc
-            V[i + offset] = V1  + Acc * i
-            v = V[i + offset]
-            S[i + offset] = S2 - (v + V2) * (T2 - i) / 2
-    offset += int(point[0])
+    print(type(points[0,0]))
 
-pp.figure()
-pp.subplot(3,1,1)
-pp.plot(time, V, label="Velocity")
-pp.ylabel('Velocity')
-pp.subplot(3,1,2)
-pp.plot(time, S, color="orange", label="Position")
-pp.xlabel('Time')
-pp.ylabel('Position')
-pp.subplot(3,1,3)
-pp.plot(time, A, color="green", label="Acceleration")
-pp.xlabel('Time')
-pp.ylabel('Acceleration')
-#pp.legend(loc="right")
-pp.show()
+    time = np.arange(0, np.sum(Tcol))
+    A = np.zeros_like(time, dtype=float)
+    V = np.zeros_like(time, dtype=float)
+    P = np.zeros_like(time, dtype=float)
+    V2 = 0
+    offset = 0
+    for k in range(0, len(Tcol)):
+        V1 = V2
+        V2 = Vcol[k]
+        P2 = Pcol[k]
+        T1 = 0
+        T2 = Tcol[k]
+        if (T2 > T1):
+            Acc = (V2 - V1) / (T2 - T1)
+            for i in range(0, int(Tcol[k])) :
+                A[i + offset] = Acc
+                V[i + offset] = V1  + Acc * i
+                v = V[i + offset]
+                P[i + offset] = P2 - (v + V2) * (T2 - i) / 2
+        offset += int(Tcol[k])
+
+    print(points)
+    pp.figure()
+    pp.subplot(3,1,1)
+    pp.plot(time, V, label="Velocity")
+    pp.ylabel('Velocity')
+    pp.subplot(3,1,2)
+    pp.plot(time, P, color="orange", label="Position")
+    pp.xlabel('Time')
+    pp.ylabel('Position')
+    pp.subplot(3,1,3)
+    pp.plot(time, A, color="green", label="Acceleration")
+    pp.xlabel('Time')
+    pp.ylabel('Acceleration')
+    pp.show()
--- firmware/sigmadrive/velocityprofiler/trapezoidal-profile.cpp
+++ firmware/sigmadrive/velocityprofiler/trapezoidal-profile.cpp
@@ -9,67 +9,75 @@
 
 #define SQ(x) ((x) * (x))
 
-
-std::vector<std::vector<int64_t>> CalculateTrapezoidPoints(float Xf, float Xi, float Vin, float Vmax, float Amax, float Dmax, float Hz)
+// Pin   - initial position              [ec]
+// Pfin  - final position                [ec]
+// Vin   - initial velocity              [ec/sec]
+// Vfin  - final velocity                [ec/sec]
+// Vmax  - max velocity allowed          [ec/sec]
+// Accel - acceleration                  [ec/(sec*sec)]
+// Decel - deceleration                  [ec/(sec*sec)]
+// Hz    - Closed loop update frequency  [Hz]
+std::vector<std::vector<int64_t>> CalculateTrapezoidPoints(float Pin, float Pfin, float Vin, float Vfin, float Vmax, float Accel, float Decel, float Hz)
 {
-	float s = (Xf >= Xi) ? 1.0f : -1.0f;
+	float s = (Pfin >= Pin) ? 1.0f : -1.0f;
 	float Vr = abs(Vmax) / Hz;				// Requested Velocity
-	float Ar = abs(Amax) / SQ(Hz);			// Requested Acceleration
-	float Dr = abs(Dmax) / SQ(Hz);			// Requested Deceleration
-	float dX = abs(Xf - Xi);
-	float Vi = s * Vin  / Hz;
+	float Ar = abs(Accel) / SQ(Hz);			// Requested Acceleration
+	float Dr = abs(Decel) / SQ(Hz);			// Requested Deceleration
+	float dP = abs(Pfin - Pin);				// Total displacement
+	float Vi = s * Vin  / Hz;				// Initial speed in encoder counts/per timeslice
+	float Vf = s * Vfin  / Hz;				// Final speed in encoder counts/per timeslice
 
 
 	if (Vi > Vr)
 		Ar = -Ar;
-	float Da = 0.5 * (Vr + Vi) * (Vr - Vi) / Ar;
-	float Dd = 0.5 * SQ(Vr) / Dr;
-	float Dc = dX - (Da + Dd);
+	float Da = 0.5 * (Vr + Vi) * (Vr - Vi) / Ar;		// Displacement during acceleration
+	float Dd = 0.5 * (Vr + Vf) * (Vr - Vf) / Dr;		// Displacement during deceleration
+	float Dc = dP - (Da + Dd);							// Displacement during const velocity
 
 	if (Dc < 0) {
 		// Find Vr by solving:
-		// Da + Dd = dX
-		// 0.5 * (Vr + Vi) * (Vr - Vi) / Ar + 0.5 *(Vr * Vr) / Dr = dX
-		float Vr_sq = Dr * (2.0 * Ar * dX + SQ(Vi))/(Dr + Ar);
+		// Da + Dd = dP
+		// 0.5 * (Vr + Vi) * (Vr - Vi) / Ar + 0.5 *(Vr * Vr) / Dr = dP
+		float Vr_sq = Dr * (2.0 * Ar * dP + SQ(Vi))/(Dr + Ar);
 		if (Vr_sq < 0 or Da == 0) {
 			// The distance to the requested position is too short
 			// for the specified decelaration.
 			// Calculate the required decelaration to reach the position
 			Vr = Vi;
-			Dr = 0.5 * SQ(Vi) / dX;
+			Dr = 0.5 * SQ(Vi) / dP;
 		} else {
 			Vr = sqrtf(Vr_sq);
 		}
 		Dc = 0;
 		Da = 0.5 * (Vr + Vi) * (Vr - Vi) / Ar;
-		Dd = 0.5 * SQ(Vr) / Dr;
+		Dd = 0.5 * (Vr + Vf) * (Vr - Vf) / Dr;
 	}
 
 	float Ta = (Vr - Vi) / Ar;
 	float Tr = (Vr != 0.0f) ? Dc / Vr : 0.0f;
-	float Td = Vr / Dr;
+	float Td = (Vr - Vf) / Dr;
 
-	float pt0time = 0;
-	float pt0velocity = Vi;
-	float pt0position = Xi;
+	float pt0t = 0;
+	float pt0v = Vi;
+	float pt0p = Pin;
 
-	float pt1time = Ta;
-	float pt1velocity = s * Vr;
-	float pt1position = Xi + (Vi + pt1velocity) * Ta * 0.5;
+	float pt1t = Ta;
+	float pt1v = s * Vr;
+	float pt1p = Pin + (Vi + pt1v) * Ta * 0.5;
 
-	float pt2time = Tr;
-	float pt2velocity = s * Vr;
-	float pt2position = pt1position + (pt1velocity + pt2velocity) * Tr * 0.5;
+	float pt2t = Tr;
+	float pt2v = s * Vr;
+	float pt2p = pt1p + (pt1v + pt2v) * Tr * 0.5;
 
-	float pt3time = Td;
-	float pt3velocity = 0;
-	float pt3position = pt2position + (pt2velocity + pt3velocity) * Td * 0.5;
+	float pt3t = Td;
+	float pt3v = Vf;
+	float pt3p = Pfin;
 
 	std::vector<std::vector<int64_t>> ret = {
-		{(int64_t)pt0time, (int64_t)pt0velocity, (int64_t)pt0position},
-		{(int64_t)pt1time, (int64_t)pt1velocity, (int64_t)pt1position},
-		{(int64_t)pt2time, (int64_t)pt2velocity, (int64_t)pt2position},
-		{(int64_t)pt3time, (int64_t)pt3velocity, (int64_t)pt3position}};
+		{(int64_t)pt0t, (int64_t)pt0v, (int64_t)pt0p},
+		{(int64_t)pt1t, (int64_t)pt1v, (int64_t)pt1p},
+		{(int64_t)pt2t, (int64_t)pt2v, (int64_t)pt2p},
+		{(int64_t)pt3t, (int64_t)pt3v, (int64_t)pt3p}};
 	return ret;
 }
 
