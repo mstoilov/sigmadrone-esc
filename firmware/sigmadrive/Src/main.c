7d335a85482f2875a8d1b09d97691a3e1168e9dd
Martin Stoilov <martin@sigmadrone.org>
2023-10-18 20:15:40 +0200

    Changed TIM1_PERIOD_CLOCKS to 5000. Upgraded CubeMX version

--- firmware/sigmadrive/Src/main.c
+++ firmware/sigmadrive/Src/main.c
@@ -1376,6 +1376,8 @@ static void MX_DMA_Init(void)
 static void MX_GPIO_Init(void)
 {
   GPIO_InitTypeDef GPIO_InitStruct = {0};
+/* USER CODE BEGIN MX_GPIO_Init_1 */
+/* USER CODE END MX_GPIO_Init_1 */
 
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOE_CLK_ENABLE();
@@ -1491,6 +1493,8 @@ static void MX_GPIO_Init(void)
   HAL_NVIC_SetPriority(EXTI9_5_IRQn, 2, 0);
   HAL_NVIC_EnableIRQ(EXTI9_5_IRQn);
 
+/* USER CODE BEGIN MX_GPIO_Init_2 */
+/* USER CODE END MX_GPIO_Init_2 */
 }
 
 /* USER CODE BEGIN 4 */
